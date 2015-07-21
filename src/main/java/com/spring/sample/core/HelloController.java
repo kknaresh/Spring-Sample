d71621711f72bbcf58b4b22b070ee52257561fac
kknaresh <naresh.k@cngl.ie>
2015-07-21 14:10:58 +0100

    First Commit Added Request Handler and Controller Class

---
+++ src/main/java/com/spring/sample/core/HelloController.java
@@ -0,0 +1,19 @@
+package com.spring.sample.core;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	//Request Handler Method
+@RequestMapping("/hello")	
+
+public String hello()
+{
+	return "Hello,World";
+}
+	
+	
+
+}
